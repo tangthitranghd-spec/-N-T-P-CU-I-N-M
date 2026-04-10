a06d356d35201c3d7fe9c9bf7997837170196525
tangthitranghd-spec <tangthitranghd@gmail.com>
2026-04-10 11:53:37 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base:'/-N-T-P-CU-I-N-M/',
     plugins: [react(), tailwindcss()],
     define: {
       base:'/-N-T-P-CU-I-N-M/',
